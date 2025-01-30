dbc33d6f4927aa22d82b10aaf02129835efe5548
Luke <lce1868@g.rit.edu>
2025-01-30 01:15:11 -0500

    working on exporting history to csv and json

--- converter/src/SaveButton.js
+++ converter/src/SaveButton.js
@@ -9,15 +9,40 @@ function SaveButton(props) {
         const date = new Date().toLocaleDateString(Intl.DateTimeFormat().resolvedOptions().locale);
         if (props.method === 'JSON') {
             const link = document.createElement('a');
-            link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(localStorage.getItem('history')));
+            link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.parse(localStorage.getItem('history')));
             link.download = 'conversion_history_' + date + '.json';
             link.click();
         } else if (props.method === 'CSV') {
             const link = document.createElement('a');
-            link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(localStorage.getItem('history'));
+            link.href = 'data:text/csv;charset=utf-8,' + encodeURIComponent(jsonToCSV(JSON.parse(localStorage.getItem('history'))));
             link.download = 'conversion_history_' + date + '.csv';  
             link.click();
         }
+    }
+
+    const jsonToCSV = (json) => {
+        let csv = '';
+        const keys = Object.keys(json);
+        const outputKeys = Object.keys(json.input);
+        keys.forEach((key) => {
+            csv += key + ', ';
+        })
+        outputKeys.forEach((key) => {
+            csv += key + ', ';
+        })
+        csv += '\r\n';
+        json.forEach((item) => {
+            keys.forEach((key) => {
+                csv += item[key] + ', ';
+            })
+            outputKeys.forEach((key) => {
+                csv += item.output[key] + ', ';
+            })
+            csv += '\r\n';
+        })
+        return csv;
+
+        
     }
 
     return (
